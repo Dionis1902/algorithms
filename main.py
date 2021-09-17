2b9dae788ed5d853f5e020311260728f485b92e9
denispitkov <den70007.ua@gmail.com>
2021-09-17 09:26:59 +0300

    updated

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ class QueueWithPriority:
     def __init__(self):
         self.__queue = list()
 
-    def insert_with_priority(self, value, priority: int = 0):
+    def push(self, value, priority: int = 0):
         for i, element in enumerate(self.__queue):
             if priority >= element.priority:
                 self.__queue.insert(i, QueueWithPriority.__Element(value, priority))
                 return
         self.__queue.append(QueueWithPriority.__Element(value, priority))
 
-    def pull_highest_priority_element(self):
+    def pop(self):
         if self:
             return self.__queue.pop(0).value
         return None
 
-    def get_highest_priority_element(self):
+    def peek(self):
         if self:
             return self.__queue[0].value
         return None
@@ -43,11 +43,11 @@ class QueueWithPriority:
 
 if __name__ == '__main__':
     queue = QueueWithPriority()
-    queue.insert_with_priority(23, 4)
-    queue.insert_with_priority(56, 0)
-    queue.insert_with_priority(567, 2)
-    queue.insert_with_priority(346, 5)
-    queue.insert_with_priority(679, 2)
-    queue.insert_with_priority(45746, 45)
+    queue.push(23, 4)
+    queue.push(56, 0)
+    queue.push(567, 2)
+    queue.push(346, 5)
+    queue.push(679, 2)
+    queue.push(45746, 45)
     for element in queue:
         print(element)
--- test.py
+++ test.py
@@ -5,21 +5,21 @@ from main import QueueWithPriority
 class TestQueue(unittest.TestCase):
     def setUp(self):
         self.queue = QueueWithPriority()
-        self.queue.insert_with_priority(3, 5)
-        self.queue.insert_with_priority(7, 2)
-        self.queue.insert_with_priority(433, 7)
-        self.queue.insert_with_priority(323, 0)
+        self.queue.push(3, 5)
+        self.queue.push(7, 2)
+        self.queue.push(433, 7)
+        self.queue.push(323, 0)
 
     def test_get(self):
-        self.assertEqual(self.queue.get_highest_priority_element(), 433)
+        self.assertEqual(self.queue.peek(), 433)
 
     def test_pull(self):
-        self.assertEqual(self.queue.pull_highest_priority_element(), 433)
+        self.assertEqual(self.queue.pop(), 433)
         self.assertEqual(len(self.queue), 3)
 
     def test_insert(self):
-        self.queue.insert_with_priority(124, 44)
-        self.assertEqual(self.queue.get_highest_priority_element(), 124)
+        self.queue.push(124, 44)
+        self.assertEqual(self.queue.peek(), 124)
 
 
 if __name__ == '__main__':
